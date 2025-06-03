00979f02a781ec8e7e9d918a7d2351592a8eb442
Dilan <dilansalas200@gmail.com>
2025-06-03 09:39:05 -0600

    cambios

--- quejas/js/quejaFunciones.js
+++ quejas/js/quejaFunciones.js
@@ -1,7 +1,7 @@
 // quejaFunciones.js
 const form = document.getElementById("formQueja");
 const mensaje = document.getElementById("mensaje");
-const apiBase = "https://metodos-metodosapi-es6noa-e36110-145-223-74-28.traefik.me/quejas/";
+const apiBase = "https://metodos-metodosapi-es6noa-e36110-145-223-74-28.traefik.me/quejas";
 let chartC = null;
 let chartPie = null;
 let promedioAlert = document.getElementById("alert-promedio");
